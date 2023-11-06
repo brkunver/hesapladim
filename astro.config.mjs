7334eff878009eb327c486cfe8842104423caa48
burakhanunver <burakhanunver@gmail.com>
2023-11-06 13:50:17 +0300

    fresh init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-
-import react from "@astrojs/react"
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
-})
+export default defineConfig({});
