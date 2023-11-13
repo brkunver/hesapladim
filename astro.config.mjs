628055fc0f658c93878eb1bb2242c5a59645f25d
Burak <burakhanunver@gmail.com>
2023-11-13 18:10:45 +0300

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kolaycahesapla.com.tr",
-  integrations: [
-    tailwind(),
-    sitemap({
-      filter: (page) => page !== "https://stargazers.club/404",
-    }),
-  ],
-})
+  integrations: [tailwind(), sitemap({
+    filter: page => page !== "https://stargazers.club/404"
+  }), partytown()]
+});
