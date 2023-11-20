4e8b7ced3228fc7330b4bb9307955e097f059b47
Burak <burakhanunver@gmail.com>
2023-11-20 12:39:29 +0300

    degisim

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kolaycahesapla.com.tr",
+  site: "https://hesapladim.com.tr",
   integrations: [
     tailwind(),
     sitemap({
-      filter: (page) => page !== "https://kolaycahesapla.com.tr/404",
+      filter: (page) => page !== "https://hesapladim.com.tr/404",
     }),
   ],
 })
